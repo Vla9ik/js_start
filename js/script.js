f9b3b0b9f3a61b114200422feceec70e198e6aac
Vlad <vla9ik2020@gmail.com>
2022-09-27 21:46:35 +0300

    add method and change writeourenres

--- js/script.js
+++ js/script.js
@@ -44,10 +44,22 @@ const personalMovieDB = {
             console.log(personalMovieDB);
         }
     },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
     writeYourGenres: function() {
         for (let i = 1; i <= 3; i++) {
-            const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-            personalMovieDB.genres[i - 1] = genre;
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (genre === '' || genre === null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
         }
     }
 };
